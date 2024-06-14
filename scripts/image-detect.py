cdcc05d604d8e66f66f75554c048c5a027c5006c
Mihaly Kocsis <kocsismhly@gmail.com>
2024-06-14 17:06:19 +0200

    Detect boats from images

---
+++ scripts/image-detect.py
@@ -0,0 +1,62 @@
+import torch
+from pathlib import Path
+import cv2
+import argparse
+import random
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='runs/train/boats/weights/best.pt')
+class_names = ['cruise ship', 'ferry boat', 'freight boat', 'gondola', 'inflatable boat', 'kayak', 'paper boat',
+               'sailboat', 'buoy']
+
+
+def draw_boxes_cv2(image, results):
+    for *box, conf, cls in results:
+        x1, y1, x2, y2 = map(int, box)
+        label = f'{class_names[int(cls)]} {conf:.2f}'
+        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+    return image
+
+
+def detect_boats(image_path):
+    image = cv2.imread(image_path)
+    results = model(image_path)
+    predictions = results.xyxy[0].numpy()
+    image_with_boxes = draw_boxes_cv2(image, predictions)
+    cv2.imshow('Detected Boats', image_with_boxes)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def detect_random_images(directory):
+    image_files = list(Path(directory).glob('*.jpg'))
+    while True:
+        image_path = random.choice(image_files)
+        image = cv2.imread(str(image_path))
+        results = model(str(image_path))
+        predictions = results.xyxy[0].numpy()
+        image_with_boxes = draw_boxes_cv2(image, predictions)
+        cv2.imshow('Detected Boats', image_with_boxes)
+        key = cv2.waitKey(0)
+        if key == ord('q'):
+            break
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Detect boats in images using YOLOv5.')
+    parser.add_argument('--image', type=str, help='Path to a specific image file')
+    parser.add_argument('--random', action='store_true',
+                        help='Display predictions for random images from the validation folder')
+
+    args = parser.parse_args()
+
+    if args.random:
+        print('Press \'space\' to see the next image.')
+        print('Press \'q\' to quit.')
+        detect_random_images('data/boat-dataset/images/val')
+    elif args.image:
+        print('Press \'q\' to quit.')
+        detect_boats(args.image)
+    else:
+        print('Please provide an image path with --image or use --random to display random images.')
